d631d479edc0b42c7042f9cf64373f84714d7deb
Danilo Jesus <hammerdevofc@gmail.com>
2025-06-22 17:12:51 -0300

    Nível Novato

--- xadrez.c
+++ xadrez.c
@@ -2,5 +2,27 @@
 
 int main()
 {
+	int torreMov = 5;
+	int bispoMov = 5;
+	int rainhMov = 8;
+
+	printf(" > Movimentação TORRE <\n");
+	for (int i = 0; i < torreMov; i++) {
+		printf("Direita\n");
+	}
+
+	printf("\n");
+
+	printf(" > Movimentação BISPO < \n");
+	for (int i = 0; i < bispoMov; i++) {
+		printf("Cima, Direita\n");
+	}
+
+	printf("\n");
+	printf(" > Movimentação RAINHA < \n");
+	for (int i = 0; i < rainhMov; i++) {
+		printf("Esquerda\n");
+	}
+
 	return 0;
 }
